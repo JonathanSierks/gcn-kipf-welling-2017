9867659578b870de841610c39661103770d38ea1
JonathanSierks <jonathan.sierks@gmail.com>
2026-02-09 13:24:22 +0100

    added GCN class

--- src/model.py
+++ src/model.py
@@ -10,5 +10,16 @@ class GCNLayer(nn.Module):
 
     def forward(self, X, A_hat):
         return torch.spmm(A_hat, (X @ self.W))
-    
+
+class GCN(nn.Module):
+    def __init__(self, n_input, n_hidden, n_output):
+        super().__init__()
+        self.layer1 = GCNLayer(n_input, n_hidden)
+        self.layer2 = GCNLayer(n_hidden, n_output)
+
+    def forward(self, X, A_hat):
+        xw_1 = self.layer1(X, A_hat)
+        act_1 = torch.relu(xw_1)
+        xw_2 = self.layer2(xw_1, A_hat)
+        return xw_2
     
